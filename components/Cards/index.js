69a071b05d9ae68d88abfdeab6d208984e4b1ee2
Mina <mramz1337@gmail.com>
2020-02-14 13:33:45 -0500

    Sprint applied javascript

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,48 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios
+    .get("https://lambda-times-backend.herokuapp.com/articles")
+    .then(response => {
+        console.log(response);
+        let cardContainer = document.querySelector(".cards-container");
+        let carddata = Object.values(response.data.articles);
+        carddata.forEach(element => {
+            element.forEach(item => {
+                cardContainer.appendChild(cardCreater(item));
+            });
+        });
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+const cardCreater = response => {
+    const newCard = document.createElement("div");
+    newCard.classList.add("card");
+
+    const headline = document.createElement("div");
+    headline.classList.add("headline");
+    headline.textContent = response.headline;
+    newCard.appendChild(headline);
+
+    const author = document.createElement("div");
+    author.classList.add("author");
+    newCard.appendChild(author);
+
+    const image = document.createElement("div");
+    image.classList.add("img-container");
+    author.appendChild(image);
+
+    const imageLink = document.createElement("img");
+    imageLink.setAttribute("src", response.authorPhoto);
+    image.appendChild(imageLink);
+
+    const authorby = document.createElement("span");
+    authorby.textContent = `By ${response.authorName}`;
+    author.appendChild(authorby);
+
+    return newCard;
+};
+console.log(cardCreater);
